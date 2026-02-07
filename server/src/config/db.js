1447216b46c6afaae60e97a05f8759892446bacf
Harsh Singh <160204410+harsh63528@users.noreply.github.com>
2026-02-07 14:37:15 +0530

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI||"mongodb+srv://vduharsh123_db_user:JLNzbSlEeta24Lzj@cluster0.j8ccbmj.mongodb.net/?appName=Cluster0");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected ");
   } catch (error) {
     console.error("Database connection failed "+ error.message);
