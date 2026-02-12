6f5d460b8771321798db55508096e4f228b4f9d7
Harsh Singh <160204410+harsh63528@users.noreply.github.com>
2026-02-12 21:21:40 +0530

    Update auth.controller.js

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -17,7 +17,7 @@ const getCookieOptions = () => {
   return {
     httpOnly: true,
     secure: isProduction,               // true in production (HTTPS)
-    sameSite: "none",
+    sameSite: isProduction?"none":"lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,    // 7 days
   };
 };
