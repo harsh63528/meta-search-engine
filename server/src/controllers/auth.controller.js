973f9487dcdfd62b66b253dc1c2cf89afd9aecb9
Harsh Singh <160204410+harsh63528@users.noreply.github.com>
2026-02-12 18:50:33 +0530

    Update auth.controller.js

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -42,7 +42,8 @@ export const registerUser = async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.STAGE === "production" ? true : false, // true in production
-    sameSite: "strict",
+      sameSite: "lax",        
+,
     maxAge: 7 * 24 * 60 * 60 * 1000
   });
 
